e92351fea39849a0275e414c37cb5c1bebe9255e
GABRIEL EDESIE <gabrieledesie@gmail.com>
2025-07-26 00:23:04 +0100

    Register Member model and customize Book admin display

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,34 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    age = models.IntegerField()
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.name
 
 
-# Book Model
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
-# Library Model
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
 
-    def __str__(self):
-        return self.name
-
-# Librarian Model
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.user.username
+
+
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    borrowed_books = models.ManyToManyField(Book, blank=True)
+
+    def __str__(self):
+        return self.user.username
