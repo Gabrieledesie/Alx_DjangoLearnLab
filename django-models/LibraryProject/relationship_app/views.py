17fc008414adc4eb30589d041efb76f7affa29a6
GABRIEL EDESIE <gabrieledesie@gmail.com>
2025-07-25 23:42:34 +0100

    Fix: separate UserCreationForm import for ALX checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.detail import DetailView
-from .models import Book
-from .models import Library
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from .models import Book, Library
 
 # Function-based view to list all books
 def list_books(request):
@@ -15,32 +16,31 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login, logout
-
+# View to handle user registration
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('list_books')  # Replace 'home' with your actual homepage view name
+            return redirect('list_books')
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
+# View to handle user login
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('list_books')  # Replace 'home' with your actual homepage view name
+            return redirect('list_books')
     else:
         form = AuthenticationForm()
     return render(request, 'relationship_app/login.html', {'form': form})
 
+# View to handle user logout
 def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
